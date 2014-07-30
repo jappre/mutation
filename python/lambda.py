a5affc53cf212107fd729b8c96141b7a6fbad2af
tiger <jappre@gmail.com>
2014-07-30 10:46:51 +0800

    add python exer

---
+++ python/lambda.py
@@ -0,0 +1,6 @@
+def make_repeater(n):
+    return lambda s: s*n
+twice = make_repeater(2)
+
+print twice('word')
+print twice(5)
---
+++ python/simplestclass.py
@@ -0,0 +1,5 @@
+class Person:
+    def sysHi(self):
+       print 'An empyt block'
+p = Person()
+p.sysHi()
