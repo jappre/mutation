5c359770625e51794f1c10fb6b7c16eba0e3731e
jappre <jappre@gmail.com>
2014-12-29 21:45:19 +0800

    feat: 练习go base

--- go/src/user/hello/hello.go
+++ go/src/user/hello/hello.go
@@ -2,45 +2,18 @@ package main
 
 import (
 	"fmt"
+	// "time"
 	//"user/newmath"
 	// "strconv"
 	// "sort"
 )
 
-type USB interface {
-	Name() string
-	Connect()
-}
-
-type PhoneConnecter struct {
-	name string
-}
-
-func (pc PhoneConnecter) Name() string {
-	return pc.name
-}
-
-func (pc PhoneConnecter) Connect() {
-	fmt.Println("Connect:", pc.name)
-}
-
 func main() {
-	var a USB
-	a = PhoneConnecter{"PhoneConnecter"}
-	a.Connect()
-	Disconnect(a)
-}
-
-func Disconnect(usb interface{}) {
-	if pc, ok := usb.(PhoneConnecter); ok {
-		fmt.Println("Disconnected", pc.name)
-		return
-	}
-
-	switch v: = usb.(type){
-		case PhoneConnecter:
-			fmt.Print("dddd")
-	}
-	fmt.Println("Unknown device. ")
-
+	c := make(chan bool)
+	go func() {
+		fmt.Println("go go go !")
+		<-c
+		// close(c)
+	}()
+	c <- true
 }
