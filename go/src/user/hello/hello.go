e4db8ee242920017f8908956109d9d1d7b944fa4
jappre <jappre@gmail.com>
2015-01-04 11:28:21 +0800

    chore: 联系gogogo

--- go/src/user/hello/hello.go
+++ go/src/user/hello/hello.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	// "time"
-	//"user/newmath"
-	// "strconv"
-	// "sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	c := make(chan bool)
-	go func() {
-		fmt.Println("go go go !")
-		<-c
-		// close(c)
-	}()
-	c <- true
+	strOrigin := ""
+	intNum := ""
+
+	_, err1 := fmt.Scanln(&strOrigin)
+	if nil == err1 {
+		fmt.Printf("strOrigin=%s", strOrigin)
+	}
+	_, err2 := fmt.Scanln(&intNum)
+	if nil == err2 {
+		fmt.Printf("intNum=%s", intNum)
+	}
+	x, _ := strconv.Atoi(intNum)
+
+	subStrs := make([][]string, 10)
+
+	for a := 0; a < x; a++ {
+		t := strings.
+	}
+
+	// subStrs := strings.SplitN(strOrigin, "", len(strOrigin)/x+1)
+	fmt.Println(subStrs)
 }
