85f2c58b4d62a8d20b6ab14c9f8fedf8ebb3fe85
jappre <jappre@gmail.com>
2015-01-06 14:14:24 +0800

    feat: 创建项目框架, bee new/ bee api

---
+++ go/src/testApiService/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "testApiService/docs"
+	_ "testApiService/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
