0eaacca2fc6a1009eddb50e1899be567d04f9817
jappre <jappre@gmail.com>
2016-03-16 16:36:36 +0800

    更新包

--- go/src/user/go_mongo/mgo_sample.go
+++ go/src/user/go_mongo/mgo_sample.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 type Person struct {
@@ -17,32 +15,49 @@ type Person struct {
 type Commit struct {
 	Person      Person
 	Date        time.Time
-	FileChanged int16
+	FileChanged []int16
 	Insertions  int16
 	Delettions  int16
 }
 
 func main() {
+	// session, err := mgo.Dial("localhost")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer session.Close()
+	//
+	// // Optional. Switch the session to a monotonic behavior.
+	// session.SetMode(mgo.Monotonic, true)
+	//
+	// c := session.DB("test").C("Commit")
+	// err = c.Insert(&Commit{Person{"Ale", "+55 53 8116 9639"}, time.Now(), []int16{2, 2, 3, 4}, 3, 6})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// result := Commit{}
+	// err = c.Find(bson.M{"date": "2016-01-27"}).One(&result)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// fmt.Println("Deletions:", result.Delettions)
+
+	saveDeliver(&Commit{Person{"Ale", "+55 53 8116 9639"}, time.Now(), []int16{2, 2, 3, 4}, 3, 6})
+}
+
+func saveDeliver(docs ...interface{}) {
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
-
 	c := session.DB("test").C("Commit")
-	err = c.Insert(&Commit{Person{"Ale", "+55 53 8116 9639"}, time.Now(), 2, 3, 6})
+	err = c.Insert(docs[0])
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	result := Commit{}
-	err = c.Find(bson.M{"date": "2016-01-27"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Deletions:", result.Delettions)
 }
--- go/src/user/server/server222.go
+++ go/src/user/server/server222.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/go-martini/martini"
+)
 
 func main() {
 	m := martini.Classic()
@@ -12,8 +18,23 @@ func main() {
 		return "Hello, Patch!"
 	})
 
-	m.Post("/", func() (int, string) {
+	m.Post("/", func(res http.ResponseWriter, req *http.Request) (int, string) {
 		// create something
+
+		// fmt.Println(req.Method)
+		// fmt.Println(req.Form["fr"])
+		// fmt.Println(req.PostForm["fr"])
+
+		req.ParseForm()
+		body_byte, err := ioutil.ReadAll(req.Body)
+		// header_byte := req.Header
+		if err != nil {
+			//statusCode = -200
+			return 400, "error"
+		}
+		body := string(body_byte)
+		fmt.Println(body, "\n")
+		fmt.Println(req.PostForm["fe"], "\n")
 		return 418, "Hello, Post!"
 	})
 
