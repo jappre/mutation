0abbdc926610c62a5b9ffcd4e6624391bf42eb8d
jappre <jappre@gmail.com>
2016-04-16 11:38:51 +0800

    更新go依赖包

--- go/src/user/mysql/mysql.go
+++ go/src/user/mysql/mysql.go
@@ -5,7 +5,8 @@ import _ "github.com/go-sql-driver/mysql"
 import "fmt"
 
 func main() {
-	db, err := sql.Open("mysql", "root:tiger@tcp(localhost:3306)/user_center")
+	// db, err := sql.Open("mysql", "root:123456@tcp(localhost:3306)/test")
+	db, err := sql.Open("mysql", "fengdevelop:dk@s3h%y7tr#c0z@tcp(182.92.158.10:3306)/f_shop")
 	if err != nil {
 		//panic(err.Error()) // proper error handling instead of panic in your app
 		fmt.Println("there is an error1!")
@@ -13,15 +14,15 @@ func main() {
 	defer db.Close()
 
 	// Prepare statement for reading data
-	stmtOut, err2 := db.Prepare("SELECT * from user")
+	stmtOut, err2 := db.Prepare("SELECT balance, expense FROM flow_statistics WHERE shop_id = ?")
 	if err2 != nil {
 		// panic(err.Error()) // proper error handling instead of panic in your app
 		fmt.Println("there is an error2!")
 	}
-	var id int
-	var email string
-	err = stmtOut.QueryRow(1).Scan(&id)
-	fmt.Printf("the id is %d, email is %s \n", id, email)
+	var expense float64
+	var balance float64
+	err = stmtOut.QueryRow("56f35e45eed0932bed09174d").Scan(&balance, &expense)
+	fmt.Printf("name is %f, event_type is %f \n", expense, balance)
 	// for stmtOut.Next() {
 	// 	var id int
 	// 	var email string
@@ -37,12 +38,12 @@ func main() {
 	// }
 
 	// var id int
-	rows, err3 := db.Query("SELECT * from user where create_time >'2014-08-28 16:00:00'")
+	// rows, err3 := db.Query("SELECT * from user where create_time >'2014-08-28 16:00:00'")
 	// err = stmtOut.QueryRow(1).Scan(&id) // WHERE number = 13
-	if err3 != nil {
-		// panic(err.Error()) // proper error handling instead of panic in your app
-		fmt.Printf("there is an error!,%v", err.Error())
-	}
+	// if err3 != nil {
+	// panic(err.Error()) // proper error handling instead of panic in your app
+	// 	fmt.Printf("there is an error!,%v", err.Error())
+	// }
 	// columns, err := rows.Columns()
 	//
 	// values := make([]sql.RawBytes, len(columns))
@@ -78,10 +79,10 @@ func main() {
 	// if err = rows.Err(); err != nil {
 	// 	panic(err.Error()) // proper error handling instead of panic in your app
 	// }
-	for rows.Next() {
-		var valuesofcolumn interface{}
-		err = rows.Scan(valuesofcolumn)
-		fmt.Printf("the id is %v", valuesofcolumn)
-	}
+	// for rows.Next() {
+	// 	var valuesofcolumn interface{}
+	// 	err = rows.Scan(valuesofcolumn)
+	// 	fmt.Printf("the id is %v", valuesofcolumn)
+	// }
 	// fmt.Printf("The square number of 13 is: %d", id)
 }
