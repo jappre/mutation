caa035a637e783aa915d32e6f17a87ac145f03a8
jappre <jappre@gmail.com>
2016-06-11 21:56:23 +0800

    添加git_sum_use_git_stats

---
+++ go/src/user/git_sum_use_git_stats/git_sum.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	// "errors"
+	"container/list"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+	"user/git_sum/go_shell"
+
+	"gopkg.in/fatih/set.v0"
+)
+
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+type Commit struct {
+	Name        string
+	Email       string
+	Date        time.Time
+	Change      string
+	FileChanged int16
+	Insertions  int16
+	Delettions  int16
+}
+
+var dirList = list.New()
+var pathSet = set.New()
+
+func main() {
+	// GetFilesInPath("/Users/tiger/start.sh")
+	filepath.Walk("/Volumes/Transcend/Company/MrWind", walkFunc)
+
+	for it := dirList.Front(); it != nil; it = it.Next() {
+		fmt.Printf("PATH IS %s", fmt.Sprint(it.Value))
+		fmt.Printf("\n")
+		// fmt.Printf("%v", GetCommit(fmt.Sprint(it.Value)))
+
+		commitSet := GetCommit(fmt.Sprint(it.Value))
+		for coit := commitSet.Pop(); coit != nil; coit = commitSet.Pop() {
+			var commit Commit
+			json.Unmarshal([]byte(coit.(string)), &commit)
+			// fmt.Printf("%v", commit)
+			// fmt.Printf("\n")
+			storeInMongo(commit, getProjectName(it.Value.(string)))
+		}
+
+	}
+
+}
+
+func getProjectName(path string) string {
+	folderNames := strings.Split(path, "/")
+	return folderNames[len(folderNames)-2]
+}
+
+func storeInMongo(commit Commit, project string) {
+	fmt.Println(project)
+	fmt.Println(commit)
+	// session, err := mgo.Dial("localhost")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer session.Close()
+	// // Optional. Switch the session to a monotonic behavior.
+	// session.SetMode(mgo.Monotonic, true)
+	//
+	// c := session.DB(project).C("Commit")
+	// err = c.Insert(commit)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+}
+
+//GetFilesInPath 用来获取某个路径下的所有文件或文件夹
+func GetFilesInPath(path string) (files []string) {
+	file, error := os.Open(path)
+
+	if error != nil {
+		fmt.Printf("An error happened")
+		return
+	}
+	fmt.Printf(file.Name())
+	defer file.Close()
+	return
+}
+
+// GitRepository 用来判断该目录是否是一个git仓库
+func GitRepository(path string) (is bool) {
+	return false
+}
+
+// GetCommitSummary 用来获取某个目录下某个人的commit概要
+func GetCommitSummary(path, user string) (insertion, deletion int) {
+	return insertion, deletion
+}
+
+func walkFunc(paths string, info os.FileInfo, err error) error {
+	if strings.HasSuffix(path.Base(paths), ".git") && info.Mode() > 1420 {
+		// return errors.New("skip this directory")
+		if !pathSet.Has(md5.Sum([]byte(paths))) {
+			pathSet.Add(md5.Sum([]byte(paths)))
+			dirList.PushFront(strings.TrimSuffix(paths, ".git"))
+		}
+		return nil
+	}
+
+	// fmt.Printf("path is %s", path.Base(paths))
+	// fmt.Printf("\n")
+	// fmt.Printf("info is %d", info.Mode())
+	// fmt.Printf("\n")
+
+	return nil
+}
+
+// Author: ws <王松@123feng.com>
+//GetCommit 用来提取commit list
+func GetCommit(path string) *set.Set {
+	var authorSet = set.New()
+	commitList, _ := goshell.GetCommandMessage(gitLogCommand(path))
+	reg := regexp.MustCompile(`(?U){.+}`)
+	// reg := regexp.MustCompile(`(?U)<.+>`)
+	authorlist := reg.FindAllString(commitList, -1)
+	for i := range authorlist {
+		authorSet.Add(authorlist[i])
+	}
+	// for it := authorSet.Pop(); it != nil; it = authorSet.Pop() {
+	// 	fmt.Println(it.(string))
+	// }
+	return authorSet
+}
+
+//GitLogCommand 用来返回获取commit log的具体命令
+func gitLogCommand(path string, dest string) string {
+	// git log | grep @123feng.com | uniq -d -i | sort
+	// return "cd " + path + "; git log | grep Author: | uniq -d -i | sort"
+	// return "cd " + path + "; git log --shortstat --since='2016-01-01' --no-merges  --pretty=format:'Author:%an,Email:%ae,date:%aI'"
+	// return "cd " + path + "; git log --shortstat --no-merges  --since='2016-01-15' --pretty=format:\"\\\"}{\\\"Name\\\":\\\"%an\\\",\\\"Email\\\":\\\"%ae\\\",\\\"Date\\\":\\\"%aI\\\",\\\"Change\\\":\\\"\" | sed 'H;$!d;g;s/\\n//g' >.tmp.count ; echo \"\\\"}\">>.tmp.count; echo {\\\";cat .tmp.count"
+	return "git_stats generate -p " + path + " -o " + dest + " -l zh_cn"
+}
---
+++ go/src/user/git_sum_use_git_stats/go_shell/go_shell.go
@@ -0,0 +1,93 @@
+package goshell
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os/exec"
+	// "time"
+)
+
+func run() {
+	cmd := exec.Command("/bin/sh", "-c", "ping 127.0.0.1")
+	_, err := cmd.Output()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err := cmd.Start(); err != nil {
+		panic(err.Error())
+	}
+
+	if err := cmd.Wait(); err != nil {
+		panic(err.Error())
+	}
+}
+
+// GetCommandMessage 用来获取命令行输出的任何内容
+func GetCommandMessage(command string) (message string, err error) {
+	// fmt.Println(command)
+	cmd := exec.Command("/bin/zsh", "-c", command)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("StdoutPipe: " + err.Error())
+		return err.Error(), err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("StderrPipe: ", err.Error())
+		return err.Error(), err
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Start: ", err.Error())
+		return err.Error(), err
+	}
+	// fmt.Printf("%p", *stderr)
+	bytesErr, err := readAll(stderr, 1024)
+	if err != nil {
+		fmt.Println("ReadAll stderr: ", err.Error())
+		return err.Error(), err
+	}
+
+	if len(bytesErr) != 0 {
+		fmt.Printf("stderr is not nil: %s", bytesErr)
+		return string(bytesErr), nil
+	}
+
+	bytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		fmt.Println("ReadAll stdout: ", err.Error())
+		return err.Error(), err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("Wait: ", err.Error())
+		return err.Error(), err
+	}
+
+	// fmt.Printf("stdout: %s", bytes)
+	return string(bytes), nil
+}
+
+func readAll(r io.Reader, capacity int64) (b []byte, err error) {
+	buf := bytes.NewBuffer(make([]byte, 0, capacity))
+	// If the buffer overflows, we will get bytes.ErrTooLarge.
+	// Return that as an error. Any other panic remains.
+	defer func() {
+		e := recover()
+		if e == nil {
+			return
+		}
+		if panicErr, ok := e.(error); ok && panicErr == bytes.ErrTooLarge {
+			err = panicErr
+		} else {
+			panic(e)
+		}
+	}()
+	// fmt.Println(len(buf))
+	_, err = buf.ReadFrom(r)
+	return buf.Bytes(), err
+}
